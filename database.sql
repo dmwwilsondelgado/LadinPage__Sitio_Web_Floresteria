6603ec91fc14cfedce9deb242f10178d61288f95
Wili Delgado <wilsondelgadomoreno@gmail.com>
2026-01-05 00:15:46 -0500

    database creacion

---
+++ database.sql
@@ -0,0 +1,6 @@
+create user "wilsondelgado"@"localhost" identified by "1102717619";
+create database bb_cacao_chucureño;
+grant all privileges on bb_cacao_chucureño.* to "wilsondelgado"@"localhost";
+flush privileges;
+show databases;
+use bb_cacao_chucureño;
