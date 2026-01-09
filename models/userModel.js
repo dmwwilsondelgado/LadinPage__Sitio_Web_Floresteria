f85a670edaa7c63af6f8107c7511ca73af392ab0
Wili Delgado <wilsondelgadomoreno@gmail.com>
2026-01-08 22:47:53 -0500

    no avazo nada por estar trabajando

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,25 @@
 //imprtamos la conenction al modelo
 import conection from "../utils/db.js";
 
-// creamos una funcion que sea usable para crear el usuario
-export const createUser = async(user)=>{
-    const{nombre,email,password,telefono} = user;
-    const[result] = await conection.execute(
-        `INSERT INTO usuarios (nombre, email, password, telefono)
-        VALUES (?, ?, ?, ?)`,
-        [nombre, email, password, telefono]
-    );
-    return result.insertId;
+// creamos una clase que sea usable para crear el usuario
+class Usuarios{
+     /**
+   * Metodo para obtener los registros de la base de datos
+   * @returns  {Array} listado de los usuarios en un arreglo
+   */
+  async getAll() {
+    try {
+        const[rows] = await conection.query("Select * from usuarios")
+        return rows;
+    } catch (error) {
+        throw new Error("Error: al obtener los usuarios");
+    }
+  }
+  async create(nombre,apellido,telefono,documento,usuario,contraseña,id_ciudad,id_genero){
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
 }
