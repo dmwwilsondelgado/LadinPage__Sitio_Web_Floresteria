c951e034f5008850aa19bcc2824272d177ced24c
Wili Delgado <wilsondelgadomoreno@gmail.com>
2026-01-16 18:24:26 -0500

    renombrando proyecto

---
+++ controller/categoriasController.js
@@ -0,0 +1,11 @@
+import categoriasModel from "../models/categoriasModel.js";
+
+class categoriasController{
+    static async getCategorias(req,res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
