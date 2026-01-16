66da6b2dfbd81deead797c3e7757dbcf7393df10
Wili Delgado <wilsondelgadomoreno@gmail.com>
2026-01-16 12:22:30 -0500

    css y html reutilizado  para avanzar en el proyecto con vistas ordenandas

--- app.js
+++ app.js
@@ -10,14 +10,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/usuarios", usuariosRoutes);
-
-// rutas
-// app.use("/ciudades", ciudadesRoutes);
-// app.use("/generos", generoRoutes);
-// app.use("/lenguajes", lenguajesRoutes);
-// app.use("/usuarios", usuariosRoutes);
-
-
 // servidor
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000/ Creando Nuevo Mundo ");
