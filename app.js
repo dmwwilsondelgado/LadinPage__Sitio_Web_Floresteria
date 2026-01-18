c68dcb7d8ffe9dd83ec3f36a1846c1bb33cf5e68
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2026-01-17 22:27:22 -0500

    create router app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import usuariosRoutes from "./routes/usuariosRoute.js"
 import authRoutes from "./routes/authRoute.js";
+import categoriasRoutes from "./routes/categoriasRoute.js";
 
 //creamos inicio 
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/usuarios", usuariosRoutes);
 app.use("/auth",authRoutes);
+app.use("/categorias",categoriasRoutes);
 // servidor
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000/ Creando Nuevo Mundo ");
