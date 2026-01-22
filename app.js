36bafea2164668e7ffa684eaea372c076c5e3c17
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2026-01-22 13:11:18 -0500

    instalamos dependecias para poder abrir el navegador con su repectivo frotend y backend restruturamos codigo y documentamos

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
-//importamos el crud
-import usuariosRoutes from "./routes/usuariosRoute.js"
+import path from "path";
+import { fileURLToPath } from "url";
+import open from "open";
+
+// importamos el crud
+import usuariosRoutes from "./routes/usuariosRoute.js";
 import authRoutes from "./routes/authRoute.js";
 import categoriasRoutes from "./routes/categoriasRoute.js";
 import tipoProductoRoute from "./routes/tipoProductoRoute.js";
 import productoRoute from "./routes/productoRoute.js";
-import imagenesProductoRoute from "./routes/imagenesProductosRoute.js";1
+import imagenesProductoRoute from "./routes/imagenesProductosRoute.js";
 
-//aca inicializamos express
+// inicializamos express
 const app = express();
-//usamos el body parser para json
+const PORT = process.env.PORT || 3000; // definimos el puerto
+
+// body parser para json
 app.use(bodyParser.json());
-//carpeta publica para correr doom en el navegador arcghivos estaticos
+
+// carpeta pública para correr archivos estáticos
 app.use(express.static("public"));
-//“Todo lo que esté en la carpeta public/uploads puede ser accedido desde la URL /uploads”.
+
+// “Todo lo que esté en la carpeta public/uploads puede ser accedido desde la URL /uploads”.
 app.use("/uploads", express.static("public/uploads"));
-//para poder recibir datos en formato urlencoded desde postman o formularios
+
+// para poder recibir datos en formato urlencoded desde postman o formularios
 app.use(express.urlencoded({ extended: true }));
-//inicializamos el app con su respectivo crud
+
+// inicializamos el app con su respectivo crud
 app.use("/usuarios", usuariosRoutes);
-app.use("/auth",authRoutes);
-app.use("/categorias",categoriasRoutes);
+app.use("/auth", authRoutes);
+app.use("/categorias", categoriasRoutes);
 app.use("/tiposdeproducto", tipoProductoRoute);
 app.use("/productos", productoRoute);
 app.use("/imagenesproducto", imagenesProductoRoute);
-// servidor
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000/ Creando Nuevo Mundo ");
+
+// para poder usar __dirname en modulos ES6
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// ruta raíz → frontend
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/html/index.html"));
+});
+
+// iniciar el servidor y abrir automáticamente el navegador
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  open(`http://localhost:${PORT}/html/index.html`);
 });
