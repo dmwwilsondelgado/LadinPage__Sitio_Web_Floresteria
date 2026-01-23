05a9a75281d7b83530776de3a605118f5dc22a0f
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2026-01-23 17:18:52 -0500

    pedidos deatalle y pedidos model me confundi en direcion crear direciones

--- models/pedidosDetalleModel.js
+++ models/pedidosDetalleModel.js
@@ -38,19 +38,6 @@ class PedidosDetalleModel{
       throw new Error("Error al eliminar detalle: " + error.message);
     }
   }
-
-  // Obtener direcciones de un usuario
-  async getDireccionesByUsuario(id_usuario) {
-    try {
-      const [result] = await connection.query(
-        "SELECT * FROM direcciones WHERE id_usuario = ?",
-        [id_usuario]
-      );
-      return result;
-    } catch (error) {
-      throw new Error("Error al obtener direcciones: " + error.message);
-    }
-  }
 }
 
 export default new PedidosDetalleModel();
