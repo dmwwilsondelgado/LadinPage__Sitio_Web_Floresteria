5fe879a98afabb7628bc98243543e131fb4f3f78
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2026-01-23 17:08:27 -0500

    create pedido model

--- models/pedidosModel.js
+++ models/pedidosModel.js
@@ -15,8 +15,19 @@ class PedidosModel {
                 `select * from pedidos where id_pedido = ?`,
                 [id]
             )
+            return result[0];
         } catch (error) {
             throw new Error('Error al obtener el pedido por id: ' + error.message);
+        }
+    }
+    async createPedido(id_usuario, id_direccion, total,estado,){
+        try {
+            const [result] = await connection.query( "INSERT INTO pedidos (id_usuario, id_direccion, total, estado) VALUES (?, ?, ?, ?)", 
+                [id_usuario, id_direccion, total, estado || "pendiente"] 
+            ); 
+            return result.insertId;
+        } catch (error) {
+            throw new Error('Error al crear el pedido: ' + error.message);
         }
     }
 }
